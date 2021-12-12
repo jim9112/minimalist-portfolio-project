bc27d9b986613df11d724a0848725716142d5697
Jimmy Hannan <jim9112@gmail.com>
2021-12-12 16:52:54 -0500

    add header and footer component

---
+++ components/Layout.tsx
@@ -0,0 +1,12 @@
+import type { ReactElement } from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
+export default function Layout({ children }: { children: ReactElement }) {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h1>This is the nav bar</h1>
+    </div>
+  );
+}
