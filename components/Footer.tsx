6c7914566f7b4f11c5b316cbdd313dd0c02c8982
Jimmy Hannan <jim9112@gmail.com>
2021-12-13 14:12:09 -0500

    start mobile footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,21 @@ import React from 'react';
 export default function Footer() {
   return (
     <div>
-      <h1>This is the footer</h1>
+      <div className='px-8 py-20 flex flex-col gap-8 items-center'>
+        <h1 className='text-primary-greyish-blue font-heading font-bold text-4xl'>
+          Interested in doing a project together?
+        </h1>
+        <button className='border-2 border-primary-greyish-blue px-8 w-max h-12 font-body text-xs'>
+          CONTACT ME
+        </button>
+      </div>
+      <footer className='bg-primary-greyish-blue text-secondary-very-light-grey flex flex-col items-center'>
+        <ul className='font-body text-xs'>
+          <li>HOME</li>
+          <li>PORTFOLIO</li>
+          <li>CONTACT ME</li>
+        </ul>
+      </footer>
     </div>
   );
 }
