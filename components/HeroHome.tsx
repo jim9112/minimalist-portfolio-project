92fdd7daf05d88a41df548cea9bfd8fc058a5e79
Jimmy Hannan <jim9112@gmail.com>
2021-12-15 15:35:54 -0500

    adjust image sizing in hero

--- components/HeroHome.tsx
+++ components/HeroHome.tsx
@@ -3,25 +3,34 @@ import Image from 'next/image';
 import downArrowsIcon from '../public/icons/down-arrows.svg';
 export default function HeroHome() {
   return (
-    <div>
-      <Image src={heroImage} alt='hero image' />
-      <div className='py-6'>
-        <p className='font-heading font-bold text-4xl text-primary-greyish-blue'>
-          Hey, I’m James and I love building beautiful websites
-        </p>
+    <div className='relative'>
+      <div className='h-[271px] md:h-[666px] overflow-hidden relative'>
+        <Image
+          src={heroImage}
+          alt='hero image'
+          layout='fill'
+          objectFit='cover'
+        />
       </div>
-      <button className='bg-primary-dark-blue flex w-[200px] h-12'>
-        <div className='bg-black w-12 flex place-content-center place-items-center'>
-          <div>
-            <Image src={downArrowsIcon} alt='arrows pointing down' />
-          </div>
-        </div>
-        <div className='w-full h-full flex place-items-center place-content-center'>
-          <p className='font-body text-secondary-very-light-grey text-xs'>
-            ABOUT ME
+      <div className='bg-secondary-very-light-grey md:w-[514px] md:absolute md:bottom-[0px] z-10'>
+        <div className='py-6 md:py-14 md:pr-28'>
+          <p className='font-heading font-bold text-4xl text-primary-greyish-blue'>
+            Hey, I’m James and I love building beautiful websites
           </p>
         </div>
-      </button>
+        <button className='bg-primary-dark-blue flex w-[200px] h-12'>
+          <div className='bg-black w-12 flex place-content-center place-items-center'>
+            <div>
+              <Image src={downArrowsIcon} alt='arrows pointing down' />
+            </div>
+          </div>
+          <div className='w-full h-full flex place-items-center place-content-center'>
+            <p className='font-body text-secondary-very-light-grey text-xs'>
+              ABOUT ME
+            </p>
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
