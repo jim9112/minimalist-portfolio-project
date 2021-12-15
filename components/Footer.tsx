02cdbf2a3fb73e9edaea2bdaa4c8e638b9beb511
Jimmy Hannan <jim9112@gmail.com>
2021-12-15 14:28:17 -0500

    style footer mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import Image from 'next/image';
+import githubIcon from '../public/icons/github.svg';
+import linkedinIcon from '../public/icons/linkedin.svg';
+import twitterIcon from '../public/icons/twitter.svg';
+import logoWhite from '../public/logo-white.svg';
 
 export default function Footer() {
   return (
@@ -11,12 +15,20 @@ export default function Footer() {
           CONTACT ME
         </button>
       </div>
-      <footer className='bg-primary-greyish-blue text-secondary-very-light-grey flex flex-col items-center'>
-        <ul className='font-body text-xs'>
+      <footer className='bg-primary-greyish-blue text-secondary-very-light-grey flex flex-col items-center py-14 gap-10'>
+        <div>
+          <Image src={logoWhite} alt='logo' />
+        </div>
+        <ul className='font-body text-xs text-center flex flex-col gap-y-8'>
           <li>HOME</li>
           <li>PORTFOLIO</li>
           <li>CONTACT ME</li>
         </ul>
+        <div className='flex gap-x-8'>
+          <Image src={githubIcon} alt='github icon' />
+          <Image src={twitterIcon} alt='twitter icon' />
+          <Image src={linkedinIcon} alt='linked in icon' />
+        </div>
       </footer>
     </div>
   );
